15b524452cbc7a623fb0c8246a897c3a97402ee7
CataVergara <catalinavergara2511@gmail.com>
2024-10-02 13:08:43 -0300

    molde para las clases

---
+++ Asignatura.py
@@ -0,0 +1,12 @@
+class Asignatura:
+    def __init__(self, nombre, codigo, creditos):
+        self.nombre = nombre
+        self.codigo = codigo
+        self.creditos = creditos
+
+
+
+    def cantidad_asignatura():
+        pass
+    def mostrar_informacion():
+        pass
--- estudiante.py
+++ estudiante.py
@@ -1,2 +1,19 @@
 class Estudiante: 
-    
+    def __init__ (self, matricula, carrera, semestre):
+        self.matricula = matricula
+        self.carrera = carrera
+        self.semestre = semestre
+
+    def cantidad_estudiantes():
+        pass
+
+
+    def estudiar():
+        pass
+
+
+
+    def presentarse():
+        pass
+
+        
